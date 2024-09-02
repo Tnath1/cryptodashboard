3818ea5585b77b7f87bef1fa012503da5a4e076f
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-09-02 12:45:36 +0100

    navs ready

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+import { MainNavComponent } from '../main-nav/main-nav.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [MainNavComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
