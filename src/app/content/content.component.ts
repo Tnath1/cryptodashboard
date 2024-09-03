49cb8cd24e040aebf13885129bb58cfc45983aa6
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-09-03 23:54:31 +0100

    second section in progress

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { MainNavComponent } from '../main-nav/main-nav.component';
 import { CurrencyBalComponent } from '../currency-bal/currency-bal.component';
+import { ExchPriceComponent } from '../exch-price/exch-price.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [MainNavComponent, CurrencyBalComponent],
+  imports: [MainNavComponent, CurrencyBalComponent,ExchPriceComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
---
+++ src/app/exch-price/exch-price.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exch-price',
+  standalone: true,
+  imports: [],
+  templateUrl: './exch-price.component.html',
+  styleUrl: './exch-price.component.scss'
+})
+export class ExchPriceComponent {
+
+}
