4b7df102daa589bfd7a662f91869f4365e8fbcca
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-09-09 00:22:09 +0100

    working on nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { ToggleSidebarService } from '../app/shared/toggle-sidebar.service';
 import { AsidenavComponent } from './asidenav/asidenav.component';
 import { ContentComponent } from './content/content.component';
 
@@ -9,8 +10,30 @@ import { ContentComponent } from './content/content.component';
   standalone: true,
   imports: [RouterOutlet, NavComponent, AsidenavComponent, ContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cryptodashboard';
+
+  isOpen = false;
+
+constructor(
+  private toggleSidebarService: ToggleSidebarService,
+  private renderer: Renderer2
+) {}
+
+ngOnInit() {
+  this.toggleSidebarService.toggleSidebar$.subscribe((isOpen) => {
+    this.isOpen = isOpen;
+
+    const sidebar = document.querySelector('div.sidebar'); // Select the sidebar
+
+    if (this.isOpen) {
+      this.renderer.addClass(sidebar, 'open'); // Add 'open' class
+    } else {
+      this.renderer.removeClass(sidebar, 'open'); // Remove 'open' class
+    }
+  });
+}
+
 }
--- src/app/asidenav/asidenav.component.ts
+++ src/app/asidenav/asidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ToggleSidebarService } from '../shared/toggle-sidebar.service';
 import { NavComponent } from '../nav/nav.component';
 
 @Component({
@@ -8,6 +9,54 @@ import { NavComponent } from '../nav/nav.component';
   templateUrl: './asidenav.component.html',
   styleUrl: './asidenav.component.scss'
 })
-export class AsidenavComponent {
+export class AsidenavComponent implements OnInit {
+
+//   isOpen = false;
+
+//   constructor(
+//     private toggleSidebarService: ToggleSidebarService,
+//     private renderer: Renderer2
+//   ) {}
+
+//   ngOnInit() {
+//     this.toggleSidebarService.toggleSidebar$.subscribe((isOpen) => {
+//       this.isOpen = isOpen;
+//       if (this.isOpen) {
+//         this.renderer.addClass(document.body, 'pen');
+//       } else {
+//         this.renderer.removeClass(document.body, 'pen');
+//       }
+//     });
+
+// }
+
+
+
+
+
+
+
+isOpen = false;
+
+constructor(
+  private toggleSidebarService: ToggleSidebarService,
+  private renderer: Renderer2
+) {}
+
+ngOnInit() {
+  this.toggleSidebarService.toggleSidebar$.subscribe((isOpen) => {
+    this.isOpen = isOpen;
+
+    const sidebar = document.querySelector('div.sidebar'); // Select the sidebar
+
+    if (this.isOpen) {
+      this.renderer.addClass(sidebar, 'open');  // Add 'open' class to sidebar
+      this.renderer.addClass(document.body, 'pen');  // Add 'pen' class to body if needed
+    } else {
+      this.renderer.removeClass(sidebar, 'open');  // Remove 'open' class from sidebar
+      this.renderer.removeClass(document.body, 'pen');  // Remove 'pen' class from body
+    }
+  });
+}
 
 }
